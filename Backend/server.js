6a6ce921ea6d79eff8b66576769e61591c5302c1
Pranjali Mishra <pranjali2413067@akgec.ac.in>
2026-08-22 17:08:39 +0530

    prepare for render

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
+
 const http = require('http');
+const path = require('path');
+const express = require('express');
+
 const app = require('./app');
 const connectDB = require('./src/config/database');
 const { initSocket } = require('./src/socket');
-const express = require("express")
+
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-app.use(express.static("public"))
+// Serve React frontend
+app.use(express.static(path.join(__dirname, 'public')));
+
+// React Router fallback
+app.get('/{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 connectDB().then(() => {
     initSocket(server);
+
     server.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,12 @@ FROM node:20-alpine AS frontend-builder
 
 WORKDIR /app
 
-# Copy frontend package files
 COPY frontend/package*.json ./
 
-# Install frontend dependencies
 RUN npm install
 
-# Copy frontend source code
 COPY frontend/ .
 
-# Create production build
 RUN npm run build
 
 
@@ -27,20 +23,12 @@ FROM node:20-alpine
 
 WORKDIR /app
 
-# Copy backend package files
 COPY backend/package*.json ./
 
-# Install backend dependencies
 RUN npm install --omit=dev
 
-# Copy backend source code
 COPY backend/ .
 
-# Copy React build into backend/public
 COPY --from=frontend-builder /app/dist ./public
 
-# Application port
-EXPOSE 8000
-
-# Start backend
 CMD ["node", "server.js"]
